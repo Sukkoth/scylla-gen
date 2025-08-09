4422b14ad1b78e127b0bc0b6aac620b67118d38f
sukkoth <suukootj@gmail.com>
2025-08-09 09:20:18 +0300

    feat: format the models after generation using prettier

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 import 'dotenv/config';
 import { generateTypesAndMappers } from './generate';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 async function main() {
   try {
     await generateTypesAndMappers();
     console.log('Model generation completed!');
+
+    // Run Prettier and wait for it to finish
+    await execAsync(
+      'pnpm prettier --write "src/cassandra-models/**/*.{ts,js}"'
+    );
+
+    console.log('Formatting completed!');
+    process.exit(0);
   } catch (err) {
     console.error('Error:', err);
     process.exit(1);
