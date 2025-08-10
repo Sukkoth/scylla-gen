f55cd4e3c6f957182d4392d110b82132fde84930
sukkoth <suukootj@gmail.com>
2025-08-11 01:19:40 +0300

    feat: move MAPPER to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,32 @@ export const DB_DEFAULT_KEYSPACE = process.env.DB_DEFAULT_KEYSPACE;
 export const DB_LOCAL_DATA_CENTER = process.env.DB_LOCAL_DATA_CENTER;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
+
+export const MAPPER = {
+  ascii: 'string',
+  bigint: 'number',
+  blob: 'Buffer',
+  boolean: 'boolean',
+  counter: 'number',
+  date: 'string',
+  decimal: 'number',
+  double: 'number',
+  duration: 'string',
+  float: 'number',
+  inet: 'string',
+  int: 'number',
+  list: '[]',
+  map: 'object',
+  set: '[]',
+  smallint: 'number',
+  text: 'string',
+  time: 'string',
+  timestamp: 'Date',
+  timeuuid: 'string',
+  tinyint: 'number',
+  tuple: 'tuple',
+  uuid: 'string',
+  varchar: 'string',
+  varint: 'number',
+  vector: 'vector',
+};
