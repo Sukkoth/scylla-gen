8eeadd7dd9033f8441e1373aa23390a74d0aa6ac
sukkoth <suukootj@gmail.com>
2025-08-16 10:34:21 +0300

    refactor: make use of reusable functions

--- src/init.ts
+++ src/init.ts
@@ -1,36 +1,28 @@
 import path from 'path';
 import { writeFileSafely } from './utils';
+import { dbModelContent, typeContent } from './content';
 
 export async function init() {
-  const filePath = path.resolve(process.cwd(), 'src/models/db-client.ts');
+  const dbClientFilePath = path.resolve(
+    process.cwd(),
+    'src/models/db-client.ts',
+  );
+  const typeFilePath = path.resolve(process.cwd(), 'src/models/types.ts');
 
   console.log('Initializing scylla-gen...');
-  await writeClient(filePath);
-  const relativePath = 'src/models/db-client.ts';
-  const absolutePath = path.resolve(process.cwd(), relativePath);
+  await writeClient(dbClientFilePath, dbModelContent);
+  await writeClient(typeFilePath, typeContent);
 
   console.log(
-    `DB client is initialized at \x1b]8;;file://${absolutePath}\x07${relativePath}\x1b]8;;\x07`,
+    `DB client is initialized at \x1b]8;;file://${dbClientFilePath}\x07${dbClientFilePath}\x1b]8;;\x07`,
+  );
+  console.log(
+    `Types are initialized at \x1b]8;;file://${typeFilePath}\x07${typeFilePath}\x1b]8;;\x07`,
   );
 
   process.exit(0);
 }
 
-async function writeClient(filePath: string) {
-  const content = `import cassandra from 'cassandra-driver';
-
-export const dbClient = new cassandra.Client({
-  contactPoints: process.env.DB_CONTACT_POINTS?.split(','),
-  keyspace: process.env.DB_DEFAULT_KEYSPACE,
-  localDataCenter: process.env.DB_LOCAL_DATA_CENTER,
-  credentials: {
-    username: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-  },
-  protocolOptions: {
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  },
-});
-`;
+async function writeClient(filePath: string, content: string) {
   await writeFileSafely(filePath, content);
 }
